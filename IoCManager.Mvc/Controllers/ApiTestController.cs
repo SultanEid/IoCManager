6a9f00148bac4e77838b77c689005e8ff8a2ef89
THMAER91 <thamer8001@gmail.com>
2026-02-02 18:11:58 +0300

    add IoCManager.Mvc project folder

---
+++ IoCManager.Mvc/Controllers/ApiTestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IoCManager.Mvc.Controllers
+{
+    public class ApiTestController : Controller
+    {
+        private readonly IHttpClientFactory _factory;
+
+        public ApiTestController(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _factory.CreateClient("IocApi");
+
+            // هذا endpoint موجود عندك حسب Swagger
+            var response = await client.GetAsync("/api/Iocs");
+
+            return Content(
+                $"StatusCode: {(int)response.StatusCode} - {response.StatusCode}"
+            );
+        }
+    }
+}
